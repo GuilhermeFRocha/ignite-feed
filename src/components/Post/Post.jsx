b48e9c439591f784d094a69a5acc8b12a3b06afa
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-07-06 16:47:01 -0300

    Post comment

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -25,6 +25,14 @@ export const Post = () => {
           <a href="#">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe deu feedback</strong>
+        <textarea placeholder="Deixe um comentario" />
+        <footer>
+        <button type="submit">Publicar</button>
+        </footer>
+      </form>
      </article>
     );
 };
