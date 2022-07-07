62ea4910069d4d7d1e34475327a5c677911a942d
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-07-07 15:29:05 -0300

    map array

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,43 @@ import { Sidebar } from "./components/Sidebar/Sidebar";
 import styles from "./App.module.css";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      name: "Julio Alcantara",
+      avatar: "https://github.com/GuilhermeFRocha.png",
+      role: "Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifolio",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2020-01-01 10:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      name: "Guilherme",
+      avatar: "https://github.com/guilherme.png",
+      role: "Dev",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifolio",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2020-02-08 20:00:00"),
+  },
+];
+
 export const App = () => {
   return (
     <div>
@@ -16,7 +53,7 @@ export const App = () => {
         <Sidebar />
 
         <main>
-          <Post infos={"testes"} />
+          <Post allPosts={posts} />
         </main>
       </div>
     </div>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -5,48 +5,74 @@ import { Comment } from "../Comment/Comment";
 // Styles
 import styles from "./Post.module.css";
 
-export const Post = () => {
-  return (
-    <article className={styles.post}>
-      <header>
-        <div className={styles.author}>
-          <Avatar src="https://github.com/GuilhermeFRocha.png" alt="perfil" />
-          <div className={styles.authorInfo}>
-            <strong>Guilherme Freitas</strong>
-            <span>Web</span>
+// Libs
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+export const Post = ({ allPosts }) => {
+  return allPosts.map((post) => {
+    const publishedDateFormatted = format(
+      post.publishedAt,
+      "d 'de' LLLL 'ás' HH:mm'h'",
+      {
+        locale: ptBR,
+      }
+    );
+
+    const publishedDateRelativeToNow = formatDistanceToNow(post.publishedAt, {
+      locale: ptBR,
+      addSuffix: true,
+    });
+
+    return (
+      <article className={styles.post} key={post.id}>
+        <header>
+          <div className={styles.author}>
+            <Avatar src={post.author.avatar} alt="perfil" />
+            <div className={styles.authorInfo}>
+              <strong>{post.author.name}</strong>
+              <span>{post.author.role}</span>
+            </div>
           </div>
+
+          <time
+            title={publishedDateFormatted}
+            dateTime={post.publishedAt.toISOString()}
+          >
+            {publishedDateRelativeToNow}
+          </time>
+        </header>
+
+        <div className={styles.content}>
+          {post.content.map((item) => {
+            if (item.type === "paragraph") {
+              return <p key={item.content}>{item.content}</p>;
+            } else if (item.type === "link") {
+              return (
+                <p>
+                  <a key={item.content} href="#">
+                    {item.content}
+                  </a>
+                </p>
+              );
+            }
+          })}
         </div>
 
-        <time title="11 de Maio às 13:13" dateTime="2022-05-11 08:12">
-          Publicado há 1h
-        </time>
-      </header>
+        <form className={styles.commentForm}>
+          <strong>Deixe deu feedback</strong>
+          <textarea placeholder="Deixe um comentario" />
+          <footer>
+            <button type="submit">Publicar</button>
+          </footer>
+        </form>
 
-      <div className={styles.content}>
-        <p>sacascascascascascasccascascascascascacascasc</p>
-        <p>ssssssssssssssssssssssssssssssssssssssssss</p>
-        <p>
-          <a href="#">ssssssssssss</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
-          <a href="#">#rocketseat</a>
-        </p>
-      </div>
-
-      <form className={styles.commentForm}>
-        <strong>Deixe deu feedback</strong>
-        <textarea placeholder="Deixe um comentario" />
-        <footer>
-          <button type="submit">Publicar</button>
-        </footer>
-      </form>
-
-      <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
-      </div>
-    </article>
-  );
+        <div className={styles.commentList}>
+          <Comment />
+          <Comment />
+          <Comment />
+        </div>
+      </article>
+    );
+  });
 };
